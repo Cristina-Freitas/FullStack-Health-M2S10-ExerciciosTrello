3f3a2868947083a0ace7d604c57d1e220e1dbc69
CrisTinaFLN <crisjf.tina@hotmail.com>
2024-08-01 00:45:16 -0300

    Ex 01 - Cadastro de Médicos Spring (início)

---
+++ src/test/java/br/senai/lab365/medicos/MedicosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senai.lab365.medicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
